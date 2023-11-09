c482e462c0d29ab882e9067bd1561632a077e534
Zukile Lubanga <zukile.81.lubanga@gmail.com>
2023-11-09 03:58:19 +0200

    transferred my code from scrimba to vscode

---
+++ README.md
@@ -0,0 +1,1 @@
+In this exercise, we're required to build a React Info Site. This will have every fact about the React's Framework history and information.
---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src="../images/react-icon-small.png" className="nav-logo" />
+            <h3 className="nav-title">ReactFacts</h3>
+            <h4 className="nav-subtitle">React Course - Project 1</h4>
+        </nav>
+    )
+}
